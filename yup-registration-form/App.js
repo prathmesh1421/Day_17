f5c51bcd77410d85b20b2cb96291a97b5c6f7898
prathmesh joshi <147234424+prathmesh1421@users.noreply.github.com>
2026-05-05 17:47:21 +0530

    Create App.js

---
+++ yup-registration-form/App.js
@@ -0,0 +1,13 @@
+import AttractiveYupForm from "./AttractiveYupForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2 className="title">Register Form</h2>
+      <AttractiveYupForm />
+    </div>
+  );
+}
+
+export default App;
