36d7a3b173134f59a5193f9f68823772ab6d0203
prathmesh joshi <147234424+prathmesh1421@users.noreply.github.com>
2026-05-05 17:43:24 +0530

    Create App.js

---
+++ instant-error-feedback/App.js
@@ -0,0 +1,12 @@
+import InstantErrorFeedbackForm from "./InstantErrorFeedbackForm";
+
+function App() {
+  return (
+    <div>
+      <h2>Instant Error Feedback Form</h2>
+      <InstantErrorFeedbackForm />
+    </div>
+  );
+}
+
+export default App;
