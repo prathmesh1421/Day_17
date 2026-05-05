bb748ca46c28f78b06d57cb74ce26f80fea328ea
prathmesh joshi <147234424+prathmesh1421@users.noreply.github.com>
2026-05-05 17:45:27 +0530

    Initialize registration form application

---
+++ registration-form/App.js
@@ -0,0 +1,14 @@
+
+import RegisterForm from "./RegisterForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2>Registration Form</h2>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
